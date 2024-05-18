60f527733b1330c1a952e237b2f77407987f4306
siavava <amittaijoel@outlook.com>
2024-05-17 23:59:49 -0400

    (update): HW6

--- project/presentation/project/scene.py
+++ project/presentation/project/scene.py
@@ -15,3 +15,32 @@ class CreateCircle(Scene):
     self.play(Create(square))  # animate the creation of the square
     self.play(Transform(square, circle))  # interpolate the square into the circle
     self.play(FadeOut(square))  # fade out animation
+
+    # show integral of f(x) = x^2
+    integral = MathTex(r"\int_{0}^{1} x^2 dx = \frac{1}{3}")
+    self.play(Create(integral))  # show the integral on screen
+    self.play(FadeOut(integral))  # fade out the integral
+
+    # category theory diagram
+    diagram = MathTex(r"\text{Sets} \rightarrow \text{Groups} \rightarrow \text{Ring}")
+    self.play(Create(diagram))  # show the diagram on screen
+    # wait for 2 seconds
+    self.wait(4)
+    self.play(FadeOut(diagram))  # fade out the diagram
+
+    # draw category-theory diagram of maps between categories (\phi, \psi)
+    myTemplate = TexTemplate()
+    myTemplate.add_to_preamble(r"\usepackage{tikz-cd}")
+    diagram = Tex(r"""
+      \begin{tikzcd}
+        A \arrow[d, "g"] \arrow[r, "f"] & B \arrow[r, "\alpha"] \arrow[d, "\gamma"] & D \arrow[d, "\beta"] \\
+        C \arrow[rru, "h"] & B' \arrow[r, "\lambda"] & D'
+      \end{tikzcd}
+    """,
+    tex_template=myTemplate)
+    
+    self.play(Create(diagram))  # show the diagram on screen
+    self.wait(4)  # wait for 4 seconds
+    self.play(FadeOut(diagram))  # fade out the diagram
+
+
